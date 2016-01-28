adeef025356542ec96e35ba26b04a04342f3a666
root <root@bkvarda-tableaudemo-5.vpc.cloudera.com>
2016-01-28 13:18:12 -0800

    Updated cloudcat scripts

--- scripts/cloudcat.sh
+++ scripts/cloudcat.sh
@@ -1,7 +1,9 @@
 ##Works for RHEL/CentOS 6
 
 #Get the CM repo
-wget https://archive.cloudera.com/cm6/redhat/6/x86_64/cm/cloudera-manager.repo
+wget https://archive.cloudera.com/cm5/redhat/6/x86_64/cm/cloudera-manager.repo
+#Copy the repo to repo dir
+cp cloudera-manager.repo /etc/yum.repos.d/
 #Install Oracle Java
 sudo yum -y install oracle-j2sdk1.7
 #Install CM embedded DB (which also installs CM server and CM daemon dependencies
